18457b407ac39e4ca136fc49e3350149ea88837a
zz@hexems.com <torykit@torykit.com>
2025-12-14 17:26:17 +0800

    feat(electron): add Electron development script and clean up configuration
    - Introduced a new development script for Electron in package.json.
    - Removed unnecessary dependencies and configurations related to Tauri.
    - Cleaned up ESLint configuration and Webpack settings for better maintainability.
    - Streamlined the Electron application structure by removing unused files and code.

--- apps/electron/forge.config.ts
+++ apps/electron/forge.config.ts
@@ -1,12 +1,5 @@
 import type { ForgeConfig } from '@electron-forge/shared-types';
-import { MakerSquirrel } from '@electron-forge/maker-squirrel';
-import { MakerZIP } from '@electron-forge/maker-zip';
-import { MakerDeb } from '@electron-forge/maker-deb';
-import { MakerRpm } from '@electron-forge/maker-rpm';
-import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
-import { FusesPlugin } from '@electron-forge/plugin-fuses';
-import { FuseV1Options, FuseVersion } from '@electron/fuses';
 
 import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
@@ -16,14 +9,7 @@ const config: ForgeConfig = {
     asar: true,
   },
   rebuildConfig: {},
-  makers: [
-    new MakerSquirrel({}),
-    new MakerZIP({}, ['darwin']),
-    new MakerRpm({}),
-    new MakerDeb({}),
-  ],
   plugins: [
-    new AutoUnpackNativesPlugin({}),
     new WebpackPlugin({
       port: 3002,
       loggerPort: 3003,
@@ -41,17 +27,6 @@ const config: ForgeConfig = {
           },
         ],
       },
-    }),
-    // Fuses are used to enable/disable various Electron functionality
-    // at package time, before code signing the application
-    new FusesPlugin({
-      version: FuseVersion.V1,
-      [FuseV1Options.RunAsNode]: false,
-      [FuseV1Options.EnableCookieEncryption]: true,
-      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
-      [FuseV1Options.EnableNodeCliInspectArguments]: false,
-      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
-      [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
 };
